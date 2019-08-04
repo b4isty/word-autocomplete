14645e3451933c5262b137acdfb3975632814317
b4isty <baishakhidasgupta24@gmail.com>
2019-08-04 18:21:57 +0530

    Vocabulary model created

--- search/admin.py
+++ search/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Vocabulary
 # Register your models here.
+
+admin.site.register([Vocabulary])
--- search/models.py
+++ search/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Vocabulary(models.Model):
+    word = models.CharField(max_length=50, null=True)
+    frequency = models.IntegerField(null=True)
